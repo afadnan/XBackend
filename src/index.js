57ef5e26a9bd00b07fcd32bb05e2cac4f610def0
Adnan Ashraf <af.adnan23@gmail.com>
2024-09-17 22:11:39 +0530

    Mongo DB Connected

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ const app = express ();
 
 app.listen(3000,async()=>{
     console.log("server is running on port 3000");
+    mongoose.connect(`mongodb+srv://afadnan23:afadnan23@xproject.o64x8.mongodb.net/`);
+    console.log("Mongo database connected");
     
 });
